26b8a8abb9e9773520859b16ada25f9e23156a2d
Schlabauke <annabelle.jeffre@gmail.com>
2021-11-06 10:06:57 +0100

    Project Designshop

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+let burgerMe = document.getElementById("burgerMe")
+let mobileMenue = document.getElementById("mobileMenue")
+
+
+function openMenue() {
+    mobileMenue.style = "height:(100%)", "opacity:1"
+}
